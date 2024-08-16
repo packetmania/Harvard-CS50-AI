61d2767f6b10b9d141f85a37be29524e2b959308
Zixi <zixiruoxue@gmail.com>
2024-08-16 16:31:17 -0700

    Implemented sample_pagerank(). Now all test cases pass. Verified with check50 tool. Style50 also shows "Looks good!"

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -11,10 +11,10 @@ def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python pagerank.py corpus")
     corpus = crawl(sys.argv[1])
-    # ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
-    # print(f"PageRank Results from Sampling (n = {SAMPLES})")
-    # for page in sorted(ranks):
-    #     print(f"  {page}: {ranks[page]:.4f}")
+    ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
+    print(f"PageRank Results from Sampling (n = {SAMPLES})")
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
     ranks = iterate_pagerank(corpus, DAMPING)
     print(f"PageRank Results from Iteration")
     for page in sorted(ranks):
@@ -57,7 +57,20 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+    pagelinks = corpus[page]
+    L = len(pagelinks)
+    N = len(corpus)
+    pageprobs = {p: 1 / N for p in corpus}  # default no outgoing links
+    no_damping_factor = 1 - damping_factor
+
+    if L > 0:
+        damping_term = damping_factor / L
+        for p in corpus:
+            pageprobs[p] *= no_damping_factor
+            if p in pagelinks:
+                pageprobs[p] += damping_term
+
+    return pageprobs
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -69,7 +82,26 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    N = len(corpus)
+    pagelist = []  # { page1, page2, ... }
+    pagecount = {}  # { page1 : count1, ... }
+    tranprobs = {}  # { page1 : {page1 : prob1, page2 : prob2, ... }, ... }
+
+    for p in corpus:
+        pagelist.append(p)
+        pagecount[p] = 0
+        # Pre-generate the probability distribution with page as the key
+        tranprobs[p] = transition_model(corpus, p, damping_factor)
+
+    p = random.choice(pagelist)  # Choose the first sample page randomly
+    pagecount[p] += 1
+
+    for i in range(n-1):
+        probs = tranprobs[p]
+        p = random.choices(list(probs.keys()), weights=probs.values())[0]
+        pagecount[p] += 1
+
+    return {p: v/n for p, v in pagecount.items()}
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -116,13 +148,14 @@ def iterate_pagerank(corpus, damping_factor):
                     summation += pageranks[i] / numlinks[i]
             pageranks[p] = (1 - damping_factor) / N + damping_factor * summation
 
-            # Update convergence cflag
+            # Update convergence flag
             if abs(last_rank - pageranks[p]) <= 0.001:
-                pageconv[p] = 0
+                pageconv[p] = 0  # Converged
             else:
-                pageconv[p] = 1
+                pageconv[p] = 1  # Not converged yet
 
         if sum(pageconv.values()) == 0:
+            # All page ranks are converged with accuracy within <= 0.001
             break
 
     return pageranks
